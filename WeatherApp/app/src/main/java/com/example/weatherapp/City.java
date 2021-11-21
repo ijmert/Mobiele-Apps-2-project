13cf3ce1377a359b1445661408a1eca5baa829fe
ijmert <ijmertu@gmail.com>
2021-11-21 23:12:34 +0100

    1ste commit

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/City.java
@@ -0,0 +1,43 @@
+package com.example.weatherapp;
+
+import java.util.List;
+
+public class City {
+    String country;
+    String name;
+    String lat;
+    String lng;
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+}
